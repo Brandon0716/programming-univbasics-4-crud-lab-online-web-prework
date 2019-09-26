689b54c723f5a10f879404b4a5a72d8bc84a0291
essential-robot-4364 <brandon.james414@gmail.com>
2019-09-26 19:03:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+   array= ["I","am","loving","it"]<<
 end
 
 def add_element_to_start_of_array(array, element)
-  
+   array= ["I","am","loving","it"] .unshift
 end
 
 def remove_element_from_end_of_array(array)
