e312c95a1fb07eab396bbe0b0ef33a2237b2a686
essential-robot-4364 <brandon.james414@gmail.com>
2019-09-26 19:01:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
- array= ["I","am","loving","it"]
+[]
 end
 
 def create_an_array
-  
+   array= ["I","am","loving","it"]
 end
 
 def add_element_to_end_of_array(array, element)
