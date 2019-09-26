b3991331f9b3b7e97c996b764a69503c27048bbc
essential-robot-4364 <brandon.james414@gmail.com>
2019-09-26 19:09:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,9 +31,9 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-   array= ["I","am","loving","it"] index_number =
+   array= ["I","am","loving","it"] index_number = [4]
 end
 
 def update_element_from_index(array, index_number, element)
-
+ array= ["I","am","loving","it"]
 end
